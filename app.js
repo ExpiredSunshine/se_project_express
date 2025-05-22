17a3538d8ee6681abde678f4c844879e9b5e67c2
A.J. Croft <acroft41@yahoo.com>
2025-05-22 17:28:42 -0500

    implement database and create schemas and models

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const { PORT = 3001 } = process.env;
 const app = express();
 app.use(express.json());
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error", err));
+
 // (Later: import routes here)
 
 app.listen(PORT, () => {
